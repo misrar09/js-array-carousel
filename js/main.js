71baed4b78fb77a7b57609cc4498108ae65a6ebe
misrar09 <misrar09@gmail.com>
2023-11-02 18:17:00 +0100

    up btn Done

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
 const imageGallery = [ "./consegna/img/01.webp", "./consegna/img/02.webp", "./consegna/img/03.webp", "./consegna/img/04.webp", "./consegna/img/05.webp" ];
 
-const contaiiner = document.querySelector(".container");
+const container = document.querySelector(".container");
 
 
 for (let i = 0; i < imageGallery.length; i++) {
-    const imageList= imageGallery[i];
-    
+    const imageLink= imageGallery[i];
     const imgElement = document.createElement("img");
-    imgElement.src = imageList;
-    contaiiner.appendChild(imgElement)
+    imgElement.src = imageLink;
+    container.appendChild(imgElement)
+
+  
 
     if(i == 0) {
-        imgElement.classList.add("InAction")
+        imgElement.classList.add("inAction")
     }
 
 }
+
+const imgElements = container.querySelectorAll("img");
+
+let activeImage = 0;
+
+imgElements[activeImage].classList.add("inAction");
+
+const btnUpElement = document.querySelector(".btnUp");
+
+btnUpElement.addEventListener("click", function() {
+    if (activeImage < imgElements.length - 1) {
+        imgElements[activeImage].classList.remove("inAction");
+        activeImage++;
+        imgElements[activeImage].classList.add("inAction");
+    }
+});
