f6f27a218ca9d85f898abf86c3bf85d585059380
misrar09 <misrar09@gmail.com>
2023-11-02 17:25:27 +0100

    right code to insert images JS

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 const imageGallery = [ "./consegna/img/01.webp", "./consegna/img/02.webp", "./consegna/img/03.webp", "./consegna/img/04.webp", "./consegna/img/05.webp" ];
 
+const contaiiner = document.querySelector(".container");
 
 
-
-let imageForHtml = "";
 for (let i = 0; i < imageGallery.length; i++) {
-    const imageArray = imageGallery[i];
-    imageForHtml += `<img src="${imageArray}">`;
+    const imageList= imageGallery[i];
+    
+    const imgElement = document.createElement("img");
+    imgElement.src = imageList;
+    contaiiner.appendChild(imgElement)
 }
-document.querySelector(".container").innerHTML = imageForHtml;
