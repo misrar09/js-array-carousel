bf91746287f17559b9b678039fc1373e778f5c3b
misrar09 <misrar09@gmail.com>
2023-11-02 18:50:43 +0100

    formato

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-const imageGallery = [ "./consegna/img/01.webp", "./consegna/img/02.webp", "./consegna/img/03.webp", "./consegna/img/04.webp", "./consegna/img/05.webp" ];
+const imageGallery = ["./consegna/img/01.webp", "./consegna/img/02.webp", "./consegna/img/03.webp", "./consegna/img/04.webp", "./consegna/img/05.webp"];
 
 const container = document.querySelector(".container");
 
 
 for (let i = 0; i < imageGallery.length; i++) {
-    const imageLink= imageGallery[i];
+    const imageLink = imageGallery[i];
     const imgElement = document.createElement("img");
     imgElement.src = imageLink;
-    container.appendChild(imgElement)
+    container.appendChild(imgElement);
 
-  
 
-    if(i == 0) {
-        imgElement.classList.add("inAction")
+
+    if (i == 0) {
+        imgElement.classList.add("inAction");
     }
 
 }
@@ -23,20 +23,18 @@ let activeImage = 0;
 
 imgElements[activeImage].classList.add("inAction");
 
-
 const btnUpElement = document.querySelector(".btnUp");
 
-btnUpElement.addEventListener("click", function() {
+btnUpElement.addEventListener("click", function () {
     imgElements[activeImage].classList.remove("inAction");
 
     if (activeImage < imgElements.length - 1) {
         activeImage++;
-        
-    }
 
-    else{
+    }
+    else {
         activeImage = 0
-        
+
     }
 
     imgElements[activeImage].classList.add("inAction");
@@ -47,16 +45,20 @@ btnUpElement.addEventListener("click", function() {
 
 const btnDownElement = document.querySelector(".btnDown");
 
-btnDownElement.addEventListener("click", function() {
+btnDownElement.addEventListener("click", function () {
     imgElements[activeImage].classList.remove("inAction");
 
     if (activeImage > 0) {
         activeImage--;
-    } else {
-        activeImage = imgElements.length - 1; 
+
+    } 
+    else {
+
+        activeImage = imgElements.length - 1;
     }
 
     imgElements[activeImage].classList.add("inAction");
+
 });
 
 
